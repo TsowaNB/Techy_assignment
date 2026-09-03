b21861b59d6c4b37b72fee20bf8c6f58d1178a28
TsowaNB <tsowablessing95@gmail.com>
2026-09-03 22:57:17 +0100

    Initial commit

---
+++ tech.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+print("numpy: ", np.__version__ )
+print("pandas: ", pd.__version__)
+print("matplotlib: ", plt.matplotlib.__version__ )
+print("seaborn:", sns.__version__)
