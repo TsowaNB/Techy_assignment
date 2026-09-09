d3fb6ad8693636f7e76c527017a42a53abbc1cac
TsowaNB <tsowablessing95@gmail.com>
2026-09-09 15:28:28 +0100

    initial commit

---
+++ script.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# print("numpy: ", np.__version__ )
+# print("pandas: ", pd.__version__)
+# print("matplotlib: ", plt.matplotlib.__version__ )
+# print("seaborn:", sns.__version__)
+df = pd.read_csv("Housing.csv")
+# print(df.head())
+print(df.tail())
